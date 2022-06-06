c67ed3dc776b0bf416d4f55b6d296d0f5184fb75
Rafael Zimmermann <tmpfile@outlook.de>
2022-06-06 15:31:24 +0200

    Add script to apply natural scroll to all mices

---
+++ apply_natural_scroll.ps1
@@ -0,0 +1,6 @@
+# Apply Natural Scroll to all mouses connected
+
+Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0
+
+Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 |
+  ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 }
